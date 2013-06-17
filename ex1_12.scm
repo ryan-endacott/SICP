d703a53e64f4dc603e5227699623371d9fea2f8c
Ryan Endacott <rzeg24@gmail.com>
2013-06-17 18:54:48 -0500

    deletes

--- ex1_12.scm
+++
@@ -1,6 +0,0 @@
-(define (pascal row col)
-  (cond ((< row 1) 0)
-        ((< col 1) 0)
-        ((> col row) 0)
-        (else (+ (pascal (- row 1) col)
-                 (pascal (- row 1) (- col 1))))))
